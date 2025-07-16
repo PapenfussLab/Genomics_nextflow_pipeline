16526c48198d192ac7b0fb53441d7532271c71de
Peinan Zhao <zhao.p@il-n01.hpc.wehi.edu.au>
2025-07-16 14:56:37 +1000

    update sample run.sh

--- run_nf.sh
+++ run_nf.sh
@@ -6,5 +6,5 @@ nextflow run genomics_pipeline.nf \
     --metadata "<path_to_metadata>" \
     --genome "GRCh38" \
     --facetsuite "<path_to_facets-suite-dev.img>" \
-    --refDir "<path_to_refdata." \
-    --outDir "/vast/ai_projects/melanoma_depigmentation_biomarker_discovery/prachi_mel_WES/analysis/test/results" -resume
+    --refDir "<path_to_refdata>" \
+    --outDir "<path_to_outDir>" -resume
